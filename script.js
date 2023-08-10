f6dd13c5e602c7486c655760bf34b8e4bf367c68
Tommy47x <110035943+Tommy47x@users.noreply.github.com>
2023-08-10 14:30:39 +0300

    Update script.js
    -modified according the requirements of the program

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-let clickCounter = 0;
+let clickCounter = 1;
 
 function changeButtonColor() {
-    ++clickCounter
-    const buttonElement = document.getElementById('buton');
+    console.log(clickCounter)
+    ++clickCounter;
 
-    if (clickCounter === 1) {
-        buttonElement.classList.remove('btn-danger');
+    const buttonElement = document.getElementById('buton');
+    
+    if (clickCounter % 3 === 1 ) {
+        buttonElement.classList.remove('btn-warning', 'btn-success');
+        buttonElement.classList.add('btn-danger');
+        buttonElement.innerHTML = 'Red';
+    } else if (clickCounter % 3 === 2 || clickCounter === 1) {
+        buttonElement.classList.remove('btn-danger', 'btn-success');
         buttonElement.classList.add('btn-warning');
-        buttonElement.innerHTML = 'New Color: Yellow';
-    }
-    if (clickCounter > 1) {
-        buttonElement.classList.remove('btn-warning');
+        buttonElement.innerHTML = 'Yellow';
+    } else if (clickCounter % 3 === 0) {
+        buttonElement.classList.remove('btn-danger', 'btn-warning');
         buttonElement.classList.add('btn-success');
-        buttonElement.innerHTML = 'New Color: Green';
+        buttonElement.innerHTML = 'Green';
     }
 }
