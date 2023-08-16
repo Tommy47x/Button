4fe6d748a413893caa5985b94a9f36225673d418
Tommy47x <110035943+Tommy47x@users.noreply.github.com>
2023-08-16 13:30:13 +0300

    Update script.js
    -Updated due to new requirements:
    1. The if / else statements got removed, and changed with ternary operators.
    2. The ternary operators got an update, so I've cut some code lines, using an array of colors.
    3. Used an array of names that are set on the colors. Example: If the color changes from Red to Yellow, the name will be changed too.
    
    4. Added a functionality that every 10 seconds on the page, the color changes automatically. I did this using the 'setInterval' function.

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-let clickCounter = 1;
+let clickCounter = 0;
 
 function changeButtonColor() {
-    console.log(clickCounter)
-    ++clickCounter;
+    const colors = ['btn-danger', 'btn-warning', 'btn-success'];
+
+    clickCounter = (clickCounter + 1) % 3;
 
     const buttonElement = document.getElementById('buton');
+    buttonElement.className = `btn ${colors[clickCounter]}`;
     
-    if (clickCounter % 3 === 1 ) {
-        buttonElement.classList.remove('btn-warning', 'btn-success');
-        buttonElement.classList.add('btn-danger');
-        buttonElement.innerHTML = 'Red';
-    } else if (clickCounter % 3 === 2 || clickCounter === 1) {
-        buttonElement.classList.remove('btn-danger', 'btn-success');
-        buttonElement.classList.add('btn-warning');
-        buttonElement.innerHTML = 'Yellow';
-    } else if (clickCounter % 3 === 0) {
-        buttonElement.classList.remove('btn-danger', 'btn-warning');
-        buttonElement.classList.add('btn-success');
-        buttonElement.innerHTML = 'Green';
-    }
+    const colorNames = ['Red', 'Yellow', 'Green'];
+    buttonElement.innerHTML = colorNames[clickCounter];
 }
+
+setInterval(changeButtonColor, 10000);
